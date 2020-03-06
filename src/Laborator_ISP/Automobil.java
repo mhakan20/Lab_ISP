accfcfd686c77307ad1ee31a6b52420a7766e742
student <student@172.16.15.58>
2020-03-06 11:08:30 +0200

    final version

--- src/Laborator_ISP/Automobil.java
+++ src/Laborator_ISP/Automobil.java
@@ -4,16 +4,22 @@
 
 package Laborator_ISP;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /************************************************************/
 /**
  * 
  */
 public class Automobil extends Vechicul {
-	
+
 	public Automobil(Combustibil combustibil, tTransmisie cutie) {
-		this.combustibil=combustibil;
-		this.cutieViteze=cutie;
+		this.combustibil = combustibil;
+		this.cutieViteze = cutie;
 	}
+
 	/**
 	 * 
 	 */
@@ -33,8 +39,25 @@ public class Automobil extends Vechicul {
 
 	/**
 	 * 
-	 * @param numeFisier 
+	 * @param numeFisier
 	 */
 	public void salvare(String numeFisier) {
+		File logFile = new File(numeFisier);
+		PrintWriter filePrint;
+		try {
+			logFile.createNewFile();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		FileWriter testWriter;
+		try {
+			testWriter = new FileWriter(logFile);
+			filePrint = new PrintWriter(testWriter, true);
+			filePrint.println("Viteza maxima: " + this.vitezaMaxima);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
-};
+}
